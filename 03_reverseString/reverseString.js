7b465cc16fd4470f1f3b02abead03807adcefafd
Miphrae <anhquando@gmail.com>
2024-10-24 23:27:00 +0200

    Add 3rd exercise completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    reversedString = ``;
+    for (let i = inputString.length - 1; i >= 0; i--) {
+        reversedString += inputString.at(i);
+    }
+    return reversedString;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
