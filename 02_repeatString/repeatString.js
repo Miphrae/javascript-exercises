d68055845e66082437710bd9b6ae50578ba8027d
Miphrae <anhquando@gmail.com>
2024-10-24 20:58:47 +0200

    Add 2nd exercise completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(word, number) {
+    if (number == -1) {
+        return "ERROR";
+    }
+    else {
+        let result = ``;
+        for (let i = 0; i < number; i++) {
+            result += word;
+        }
+        return result;
+    }
+    
 };
 
 // Do not edit below this line
