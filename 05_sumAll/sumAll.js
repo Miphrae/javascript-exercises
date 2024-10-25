8273459d6a0ef39211de5e8ad5904e94324f48d3
Miphrae <anhquando@gmail.com>
2024-10-25 10:05:48 +0200

    Add 5th exercise completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,17 @@
 const sumAll = function() {
+    if (!Number.isInteger(arguments[0]) || !Number.isInteger(arguments[1]) || arguments[1] < 0 || arguments[0] < 0) {
+        return `ERROR`;
+    }
+ 
+    let smallNumber = Math.min(...arguments);
+    let bigNumber = Math.max(...arguments);
+    let sum = 0;
+
+    for (let i = smallNumber; i <= bigNumber; i++) {
+        sum += i;
+    }
+
+    return sum;
 
 };
 
