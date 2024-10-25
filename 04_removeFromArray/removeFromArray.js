63414b08882338dbdcc0b3a1f3ba68be88e8eb30
Miphrae <anhquando@gmail.com>
2024-10-25 09:35:31 +0200

    Add 4th exercise completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
 const removeFromArray = function() {
+    const inputArray = arguments[0];
+    const removeElements = [];
+    for (let i = 1; i < arguments.length; i++) {
+        removeElements.push(arguments[i]);
+    }
+    for (let i = 0; i < removeElements.length; i++) {
+        for (let j = inputArray.length - 1; j >= 0; j--) {
+            if (inputArray[j] === removeElements[i]) {
+                inputArray.splice(j,1);
+            }
+        }
+    }
+    return inputArray;
 };
 
 // Do not edit below this line
