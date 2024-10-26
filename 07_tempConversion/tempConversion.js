fe00c9f072859b97e729e4d81c837d6aaf9be769
Pi <148574565+Miphrae@users.noreply.github.com>
2024-10-26 12:25:13 +0200

    Update tempConversion.js
    completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
 const convertToCelsius = function() {
+    let tempFahrenheit = arguments[0];
+    let tempCelsius = ((tempFahrenheit-32)/1.8);
+    tempCelsius = parseFloat(tempCelsius.toFixed(1));
+    return tempCelsius;
 };
 
 const convertToFahrenheit = function() {
+    let tempCelsius = arguments[0];
+    let tempFahrenheit = ((tempCelsius*1.8)+32);
+    tempFahrenheit = parseFloat(tempFahrenheit.toFixed(1));
+    return tempFahrenheit;
 };
 
 // Do not edit below this line
