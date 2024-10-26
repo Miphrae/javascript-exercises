94d3dbceb98e103f51bf5ffb843bb0cdef7052d7
Pi <148574565+Miphrae@users.noreply.github.com>
2024-10-26 11:49:27 +0200

    Update leapYears.js
    completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
 const leapYears = function() {
-
+    let year = arguments[0];
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            return ((year % 400 == 0) ? true : false );
+        }
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
